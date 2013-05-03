711f448ede8ccecf3fd72182e5df763d791d9142
Troy Goode <troygoode@gmail.com>
2013-05-03 18:55:23 -0300

    fix bug that hangs non-redirected requests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 exports = module.exports = function(req, res, next){
   if(req.protocol == 'http' && !/^(localhost\:(\d+))|localhost$/.test(req.header('Host'))){
     res.redirect('https://' + req.header('Host') + req.url)
+  }else{
     next()
   }
 }
