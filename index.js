5d1cd3d0c4f3f2cdccd79f8500f9e9fa9dd8b9ec
Troy Goode <troygoode@gmail.com>
2013-05-03 19:23:49 -0300

    only redirect GETs to SSL; return a 403 for the rest

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-exports = module.exports = function(req, res, next){
-  if(req.protocol == 'http' && !/^(localhost\:(\d+))|localhost$/.test(req.header('Host'))){
-    res.redirect('https://' + req.header('Host') + req.url)
+module.exports = function(req, res, next){
+  if(req.protocol === 'http' && !/^(localhost\:(\d+))|localhost$/.test(req.header('Host'))){
+    if(req.method === 'GET'){
+      res.redirect('https://' + req.header('Host') + req.url);
+    }else{
+      res.send(403, 'SSL Required'); //OWASP recommends dropping connection instead: https://www.owasp.org/index.php/SSL_Best_Practices
+    }
   }else{
-    next()
+    next();
   }
-}
+};
